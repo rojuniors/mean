dd2567bd0474d5715f45b07236258cb2f2982b52
Ronaldo <rojuniors@MacBook-Pro-de-Gladyson.local>
2015-06-20 02:32:53 -0300

    Em dev para testes

--- server.js
+++ server.js
@@ -2,6 +2,6 @@
 var http = require('http');
 var app = require('./config/express')();
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express Server escutando na portaa ' +
+  console.log('Express Server escutando na porta' +
               app.get('port'));
 });
